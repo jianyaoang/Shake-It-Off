b5b1b7ebb0f2ec69cf29e1eb63a47be8e2a2613e
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-05-28 08:35:09 +0800

    Upload shake it off project files

--- Shake It Off/ViewController.swift
+++ Shake It Off/ViewController.swift
@@ -12,14 +12,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        createSwipeGestures()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func createSwipeGestures() {
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
     }
-
-
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+    if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+        
+            switch swipeGesture.direction {
+                
+                case UISwipeGestureRecognizerDirection.Right:
+                        println("Swiped to the right")
+                        break
+                case UISwipeGestureRecognizerDirection.Left:
+                        println("Swipe to the left")
+                        break
+                case UISwipeGestureRecognizerDirection.Up:
+                    println("Swipe up!")
+                    break
+                case UISwipeGestureRecognizerDirection.Down:
+                    println("Swipe down.")
+                    break
+                default:
+                        break
+            }
+        }
+    }
 }
+
 
